8f765a036198b8caa187f6bad0945e96ad2782b2
phildimond <phil@adgaming.com.au>
2024-08-17 13:33:11 +1000

    Initial Code Creation

---
+++ EnergySuper/MainWorker.cs
@@ -0,0 +1,24 @@
+namespace EnergySuper;
+
+public class MainWorker : BackgroundService
+{
+    private readonly ILogger<MainWorker> _logger;
+
+    public MainWorker(ILogger<MainWorker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            }
+
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
