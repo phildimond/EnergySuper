9a301dc7b8639c319b93a2f781ced05faba53464
phildimond <phil@adgaming.com.au>
2024-09-08 20:44:49 +1000

    Brought dependency DLLs into project

--- EnergySuper/obj/Debug/net8.0/EnergySuper.AssemblyInfo.cs
+++ EnergySuper/obj/Debug/net8.0/EnergySuper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47cda64318c58dcf7f74036aa14813afe1b32334")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22904ba20d4d754ab51f42d37b6739bf66c7641d")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
