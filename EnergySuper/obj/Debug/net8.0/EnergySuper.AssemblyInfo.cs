9079e3a438ed28f250b492881683a4fe5b49f99a
phildimond <phil@adgaming.com.au>
2024-09-08 20:48:12 +1000

    Moved dependent DLLs into a subdirectory

--- EnergySuper/obj/Debug/net8.0/EnergySuper.AssemblyInfo.cs
+++ EnergySuper/obj/Debug/net8.0/EnergySuper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22904ba20d4d754ab51f42d37b6739bf66c7641d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a301dc7b8639c319b93a2f781ced05faba53464")]
 [assembly: System.Reflection.AssemblyProductAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EnergySuper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
